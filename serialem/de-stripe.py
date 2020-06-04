675b39ee6e9d15407e33bfa0b730fd1d781a59a1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-06-04 13:08:19 -0600

    Arg parsing for the de-stripe script

---
+++ serialem/de-stripe.py
@@ -0,0 +1,54 @@
+'''
+Adjust brightness of mosaic tiles so they appear seamlessly connected.
+
+Setup:
+~ git clone https://github.com/jamesra/nornir-buildmanager
+~ cd nornir-buildmanager
+~ python setup.py install
+
+Run examples:
+~ python de-stripe.py V:\RawData\RC3 1              # de-stripe section 0001
+~ python de-stripe.py V:\RawData\RC3 1,2,3          # de-stripe sections 0001, 0002, 0003
+~ python de-stripe.py V:\RawData\RC3 1-21           # de-stripe all sections that exist between 0001 and 0022 (skips 0021 which is missing)
+~ python de-stripe.py V:\RawData\RC3 1-21,26-30     # de-stripe multiple ranges of sections
+
+'''
+
+import sys
+
+def parseSections(arg):
+    '''
+    Parse a list of section numbers from the given arg which may have comma-separated ranges and individual values
+    '''
+    sections = []
+
+    if ',' in arg:
+        for list_arg in arg.split(','):
+            sections += parseSections(list_arg)
+    else:
+        if '-' in arg:
+            lower, upper = [int(num) for num in arg.split('-')]
+            return range(lower, upper+1)
+        else:
+            return [int(arg)]
+
+    # Eliminate duplicates
+    return list(set(sections))
+
+
+
+if __name__ == "__main__":
+    volume_dir = ""
+    sections = []
+    if len(sys.argv) > 1:
+        volume_dir = sys.argv[1]
+    else:
+        print("First arg must specify the directory of a volume's raw data.")
+        exit()
+    if len(sys.argv) > 2:
+        sections = parseSections(sys.argv[2])
+    if len(sections) == 0:
+        print("Second arg must specify one or more sections to correct.")
+        exit()
+
+    print(sections)

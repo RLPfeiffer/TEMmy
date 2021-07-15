42c178b637e79279bae23dd6b500f2dfc28fae83
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-07-15 14:57:32 -0600

    Extract Python parts of NotificationsV2 to their own script, as functions

---
+++ macros/18-Notifications.py
@@ -0,0 +1,37 @@
+#!Python3.9
+#MacroName Notifications - Python
+import serialem as sem
+import os
+
+def SendMessage(Text):
+   MessageDir = sem.GetVariable("V2MessageDir")
+   MessagePath = sem.GetVariable("V2MessagePath")
+   # Make sure there is a message file to append to
+   os.makedirs(MessageDir, exist_ok=True)
+   # Append to the file so multiple messages aren't overwritten
+   with open(MessagePath, "a+") as MessageFile:
+      MessageFile.write("{}{}".format(Text, os.linesep))
+
+def SendStart():
+   EstimatedCaptureHours = 0
+   NumTiles = 0
+   SecondsPerTile = sem.GetVariable("SecondsPerTile")
+   ScopeName = sem.GetVariable("ScopeName")
+
+   if sem.ReportNumTableItems() < 1:
+      print("Nav table does not have items indicating total capture time.")
+   else:
+      NumTiles = sem.ReportNumMontagePieces(1)
+      
+      if NumTiles == 1:
+         print("Montage claims to have only one image. Using 1 hour as timeframe")
+         EstimatedCaptureHours = 1
+      else:
+         EstimatedCaptureTime = NumTiles * SecondsPerTile
+         EstimatedCaptureHours = EstimatedCaptureTime / (60 * 60)
+      print("Estimating {} hours to complete".format(EstimatedCaptureHours))
+
+      SendMessage("Started: Capturing {} images on {}. Estimating {} hours to complete".format(NumTiles, ScopeName, EstimatedCaptureHours))
+
+def SendStop():
+   SendMessage("Copied: {} copied from {} to DROPBOX.".format(sem.GetVariable(CaptureDir), sem.GetVariable(ScopeName)))

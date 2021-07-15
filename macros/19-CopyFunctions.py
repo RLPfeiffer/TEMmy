691366436df126f6cc8ebfdaa90b4e5e18e069f1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-07-15 17:02:14 -0600

    CopyFunctions.py fix

--- macros/19-CopyFunctions.py
+++ macros/19-CopyFunctions.py
@@ -3,17 +3,8 @@
 #include Notifications - Python
 import serialem as sem
 import shutil
+import os
 from os import path
-
-# The argument values need to be updated to match the SerialEM computer's current network mappings and a folder in the data drive before testing
-# Test the functions without side effects:
-#print(CheckSpaceForCopyDir("E:/", "Y:/Dropbox/TEMXCopy", "0976")) # TEM1
-# print(CheckSpaceForCopyDir("G:/", "Y:/TEMXCopy", "0975")) # TEM2
-
-# Test with major side effects:
-# CallFunction CopyFunctions::CopyDir
-#CopyDir("E:/", "Y:/Dropbox/TEMXCopy", "0976") # TEM1
-# print(CheckSpaceForCopyDir("G:/", "Y:/TEMXCopy", "0975")) # TEM2
 
 # Check to make sure there is enough space, then copy the contents CopySource recursively to CopyTarget.
 def CopyDir(CopySource, CopyTarget, TargetDirName):
@@ -22,17 +13,39 @@ def CopyDir(CopySource, CopyTarget, TargetDirName):
    else:
       SendMessage("Not enough space to auto-copy {} from {} to {}".format(TargetDirName, sem.GetVariable("ScopeName"), CopyTarget))
 
+# INTERNAL, called by CheckSpaceForCopyDir
+def RecursiveFolderSize(folder):
+    total_size = path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = path.join(folder, item)
+        if path.isfile(itempath):
+            total_size += path.getsize(itempath)
+        elif path.isdir(itempath):
+            total_size += RecursiveFolderSize(itempath)
+    return total_size
+
 # INTERNAL, called by CopyDir
 # Return True if there is space in the destination to copy the target directory.
 def CheckSpaceForCopyDir(CopySource, CopyTarget, TargetDirName):
    _, _, FreeSpace = shutil.disk_usage(CopyTarget)
-   _, TargetDirSize, _ = shutil.disk_usage(path.join(CopySource, TargetDirName))
+   TargetDirSize = RecursiveFolderSize(path.join(CopySource, TargetDirName))
    return FreeSpace >= TargetDirSize
 
 # INTERNAL helper function that copies a directory to CopyTarget without checking for space first.
 # (External-use functions all check before calling this one)
 def CopyDirWithoutCheckingSpace(CopySource, CopyTarget, TargetDirName):
    try:
-      shutil.copytree(path.join(CopySource, TargetDirName), CopyTarget)
-   except Error as e:
+      shutil.copytree(path.join(CopySource, TargetDirName), path.join(CopyTarget, TargetDirName))
+   except Exception as e:
       SendMessage("Error {} while attempting to copy {} from {} to {}".format(e, TargetDirName, sem.GetVariable("ScopeName"), CopyTarget))
+
+
+# The argument values need to be updated to match the SerialEM computer's current network mappings and a folder in the data drive before testing
+# Test the functions without side effects:
+#print(CheckSpaceForCopyDir("E:/", "Y:/Dropbox/TEMXCopy", "0976")) # TEM1
+# print(CheckSpaceForCopyDir("G:/", "Y:/TEMXCopy", "0975")) # TEM2
+
+# Test with major side effects:
+# CallFunction CopyFunctions::CopyDir
+#CopyDir("E:/", "Y:/Dropbox/TEMXCopy", "core_kwanEmbryos_13374") # TEM1
+# print(CheckSpaceForCopyDir("G:/", "Y:/TEMXCopy", "0975")) # TEM2

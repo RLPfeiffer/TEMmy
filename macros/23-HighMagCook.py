d6108144c9ac570ecbf246e4e2461d3e44297024
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-07-22 11:55:37 -0600

    HighMagCook/CalibrateAndRecapture python

---
+++ macros/23-HighMagCook.py
@@ -0,0 +1,5 @@
+#!Python3.9
+#MacroName HighMagCook - Python
+#include Captures - Python
+import serialem as sem
+Capture(True)
---
+++ macros/24-CalibrateAndRecapture.py
@@ -0,0 +1,5 @@
+#!Python3.9
+#MacroName CalibrateAndRecapture - Python
+#include Captures - Python
+import serialem as sem
+Capture(False)

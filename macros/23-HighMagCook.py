7781093aea20a3659d88c0a103efced7ebe656b1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-07-27 10:52:31 -0600

    fix python macro includes

--- macros/23-HighMagCook.py
+++ macros/23-HighMagCook.py
@@ -1,5 +1,9 @@
 #!Python3.9
 #MacroName HighMagCook - Python
 #include Captures - Python
+#include Snapshot - Python
+#include CopyFunctions - Python
+#include Notifications - Python
+#include WaitForStableFilament - Python
 import serialem as sem
 Capture(True)

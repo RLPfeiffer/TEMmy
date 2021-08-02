d7ad02fe96862d80bc4b3a4b3eef0d46c9cd2daf
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-08-02 16:11:25 -0600

    script I used to delete bad sp4 mosaic files

---
+++ scripts/delete-bad-files/src/delete_bad_files/Main.hx
@@ -0,0 +1,12 @@
+package delete_bad_files;
+
+import kiss.Kiss;
+import kiss.Prelude;
+import sys.FileSystem;
+import sys.io.File;
+import haxe.io.Path;
+
+using StringTools;
+
+@:build(kiss.Kiss.build())
+class Main {}

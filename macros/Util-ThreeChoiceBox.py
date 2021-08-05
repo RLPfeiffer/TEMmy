cbccdc48baabc7c222d85e45f3ac1696317540f5
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-08-05 13:26:06 -0600

    experimental ThreeChoiceBox python function

---
+++ macros/Util-ThreeChoiceBox.py
@@ -0,0 +1,16 @@
+def ThreeChoiceBox(header:str, choice1:str, choice2:str, choice3:str, prompt1:str="", prompt2:str="", prompt3:str="") -> str:
+    sem.SetVariable("header", header)
+    sem.SetVariable("prompt1", prompt1)
+    sem.SetVariable("prompt2", prompt2)
+    sem.SetVariable("prompt3", prompt3)
+    sem.SetVariable("buttons", sem.listToSEMarray([choice1, choice2, choice3]))
+    sem.Call("ThreeChoiceBox")
+    choice:int = int(sem.GetVariable("reportedValue1"))
+    if choice == 1:
+        return choice1
+    if choice == 2:
+        return choice2
+    if choice == 3:
+        return choice3
+    
+    raise ValueError("SerialEM didn't set reportedValue1 correctly")

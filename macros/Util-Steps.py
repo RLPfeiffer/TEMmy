ffb55baf8aa1f240206654080dfafd41c5aedfa6
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-10-04 13:50:54 -0600

    Useful tutorial step functions

--- macros/Util-Steps.py
+++ macros/Util-Steps.py
@@ -5,11 +5,28 @@ from typing import Any
 Step = Callable[[], None]
 
 # Types of protocol step for Tutorial.py:
+
+# Display an instruction that must be carried out manually:
 def TellOperator(message:str) -> Step:
     return lambda: OkBox(message)
 
+# Do something through SerialEM Scripting and keep going when it's finished:
 def DoAutomatically(func:Callable[[], None]) -> Step:
     def step() -> None:
         func()
         RunNextStep()
+    return step
+
+def DoNothing() -> None:
+    pass
+
+# Do a step differently depending on the scope being used
+def DependingOnScope(tem1Step:Step, tem2Step:Step) -> Step:
+    def step() -> None:
+        if ScopeName == "TEM1":
+            tem1Step()
+        elif ScopeName == "TEM2":
+            tem2Step()
+        else:
+            raise ValueError(f"ScopeName has unexpected value {ScopeName}")
     return step

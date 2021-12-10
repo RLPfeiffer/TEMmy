87d0178f877baf3fb6f4a7732387b23f08bca32e
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-12-10 15:27:23 -0700

    Tutorial prompt operator whether to clear rawdata sections from data drive

---
+++ macros/Util-ClearDataDrive.py
@@ -0,0 +1,25 @@
+from os.path import join, exists
+from shutil import rmtree
+
+def ClearDataDrive() -> None:
+    lines = []
+    with open(join(CopyPath, "rawdata.txt"), "r") as f:
+        lines = f.readlines()
+
+    new_lines = []
+    sections_to_delete = []
+    for line in lines:
+        if len(line) != 0:
+            section = line.split(" ")[0]
+            if exists(join(DataPath, section)):
+                sections_to_delete.append(section)
+            else:
+                new_lines.append(section)
+
+    if YesNoBox(f"Delete {len(sections_to_delete)} sections which have been copied to RawData?"):
+        map(rmtree, sections_to_delete)
+    else:
+        new_lines.extend(sections_to_delete)
+    
+    with open(join(CopyPath, "rawdata.txt"), "w") as f:
+        f.writelines(new_lines)

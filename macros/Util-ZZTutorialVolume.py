b08949bbb9efe831181f33d14aabae523c49f4b2
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-04-29 15:26:52 -0600

    Refactor RC3 tutorial flow for easy circle volume variants

--- macros/Util-ZZTutorialVolume.py
+++ macros/Util-ZZTutorialVolume.py
@@ -1,17 +1,17 @@
 # This file is named with a Z so it comes after every other Python function in Util files is defined
 
-def MainRC3Steps(detailed:bool, recap:bool) -> list[Step]:
+def MainVolumeSteps(investigator:str, name:str, radius:int, detailed:bool, recap:bool) -> list[Step]:
     FocusSteps = DetailedFocusSteps if detailed else FastFocusSteps
     
     return [
-        OpenLastSnapshot(recap, "Jones", "RC3", 150),
+        OpenLastSnapshot(recap, investigator, name, 150),
         TellOperatorSEM("Locate the center point at 150x, click it, and click 'Add Marker' in the navigator window."),
         DoAutomatically(lambda: MoveToNavItem(PolygonIndex)),
         DoAutomatically(Record),
         ManuallyCheckCenterPoint,
         DoAutomatically(lambda: TakeSnapshotWithNotes("", False))
-    ] + SwitchToHighMagSteps(recap, "Jones", "RC3", 600, HighMag600, SpotSize2, True, True, []) + SwitchToHighMagSteps(recap, "Jones", "RC3", 2000, HighMag2000, SpotSize1, False, True, FocusSteps) + [
-        TellOperatorSEM("In the menubar, click Navigator -> Montaging and Grids -> Add Circle Polygon. Type 125"),
+    ] + SwitchToHighMagSteps(recap, investigator, name, 600, HighMag600, SpotSize2, True, True, []) + SwitchToHighMagSteps(recap, investigator, name, 2000, HighMag2000, SpotSize1, False, True, FocusSteps) + [
+        TellOperatorSEM(f"In the menubar, click Navigator -> Montaging and Grids -> Add Circle Polygon. Type {radius}"),
         TellOperatorSEM("In the navigator window, delete every item EXCEPT FOR the formvar reference point and the circle Polygon."),
         DoAutomatically(lambda: SetMagIndex(HighMag5000)),
         TellOperatorSEM("With the circle polygon selected, check the Navigator checkboxes for 'Aquire', 'New File At Item', 'Montaged Images', 'Fit Montage to Polygon'. Make sure 'Go from center out and anchor at 2000x' is active and click ok. Then select the generated idoc file. Choose to overwrite it."),
@@ -20,9 +20,9 @@ def MainRC3Steps(detailed:bool, recap:bool) -> list[Step]:
     ] + FocusSteps + FinalSteps(detailed, False, recap)
 
 
-Steps["RC3"] = NewSpecimenSteps + LowMagCookSteps + MainRC3Steps(True, False)
-Steps["RC3 Recapture"] = NewSpecimenSteps + MainRC3Steps(True, True)
+Steps["RC3"] = NewSpecimenSteps + LowMagCookSteps + MainVolumeSteps("Jones", "RC3", 125, True, False)
+Steps["RC3 Recapture"] = NewSpecimenSteps + MainVolumeSteps("Jones", "RC3", 125, True, True)
 
-Steps["RC3 Fast"] = NewSpecimenSteps + LowMagCookSteps + MainRC3Steps(False, False)
-Steps["RC3 RecapFast"] = NewSpecimenSteps + MainRC3Steps(False, True)
+Steps["RC3 Fast"] = NewSpecimenSteps + LowMagCookSteps + MainVolumeSteps("Jones", "RC3", 125, False, False)
+Steps["RC3 RecapFast"] = NewSpecimenSteps + MainVolumeSteps("Jones", "RC3", 125, False, True)
 
